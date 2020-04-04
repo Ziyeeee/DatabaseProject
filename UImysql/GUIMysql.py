7594e00cc3449aa7f6a3fbb1b441dea4dde6c3d9
Ziye <504250110@qq.com>
2020-04-05 00:11:30 +0800

    Update GUIMysql.py
    完成连接数据库模块

--- UImysql/GUIMysql.py
+++ UImysql/GUIMysql.py
@@ -1,15 +1,29 @@
 import sys
 from GUI import *
 from PyQt5.QtGui import QIcon
+import pymysql
 
+
+class GUI(Ui_MainWindow):
+    def setupFunction(self):
+        self.connectButton.clicked.connect(self.connectDatabase)
+
+    def connectDatabase(self):
+        try:
+            self.__db = pymysql.connect(self.hostLineEdit.text(), self.userNameLineEdit.text(), self.keyLineEdit.text(),
+                                        self.databaseLineEdit.text())
+            self.__cursor = self.__db.cursor()
+            self.statusbar.showMessage("Connected")
+        except:
+            self.statusbar.showMessage("Error in connecting MySQL")
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = GUI()
     ui.setupUi(MainWindow)
-    #ui.setupFunction()             #还没有添加操作函数
+    ui.setupFunction()
     MainWindow.setWindowTitle("Database Manager")
     MainWindow.setWindowIcon(QIcon("database.png"))
     MainWindow.show()
