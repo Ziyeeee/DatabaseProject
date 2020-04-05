f0c49ed8e207b74157c195d0a2d7a3f7512744b4
Ziye <504250110@qq.com>
2020-04-05 15:16:38 +0800

    优化页面布局

--- UImysql/GUI.py
+++ UImysql/GUI.py
@@ -311,7 +311,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.functionTabWidget.setCurrentIndex(3)
+        self.functionTabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
