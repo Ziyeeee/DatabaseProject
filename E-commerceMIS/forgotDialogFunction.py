b152191a99db2af3139b6416c29b9399349b302c
Ziye <504250110@qq.com>
2020-05-07 17:36:43 +0800

    Update

---
+++ E-commerceMIS/forgotDialogFunction.py
@@ -0,0 +1,44 @@
+from forgotDialog import Ui_Dialog
+from PyQt5.QtWidgets import QWidget, QMessageBox
+import pymysql
+
+
+class forgotDialog(QWidget, Ui_Dialog):
+
+    def __init__(self, db, dbcursor, index, name, data):
+        super(forgotDialog, self).__init__()
+        self.setupUi(self)
+        self.db = db
+        self.dbcursor = dbcursor
+        self.index = index
+        self.name = name
+        self.data = data
+
+        self.que1Label.setText(self.data[0])
+        self.que2Label.setText(self.data[2])
+
+        self.pushButton.clicked.connect(self.conform)
+
+    def conform(self):
+        if self.ans1LineEdit.text() == self.data[1] and self.ans2LineEdit.text() == self.data[3] \
+                and self.keyLineEdit.text() == self.confirmLineEdit.text():
+            if self.index == 0:
+                self.sql = 'update buyer set Bkey = \"' + self.keyLineEdit.text() + '\" where Bname = \"' \
+                           + self.name + '\"'
+            elif self.index == 1:
+                self.sql = 'update store set Bkey = \"' + self.keyLineEdit.text() + '\" where Sname = \"' \
+                           + self.name + '\"'
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+            QMessageBox.information(self, 'Information', '修改成功')
+            self.close()
+
+        elif self.ans1LineEdit.text() != self.data[1] or self.ans2LineEdit.text() != self.data[3]:
+            QMessageBox.warning(self, 'Warning', '回答错误')
+            self.ans1LineEdit.clear()
+            self.ans2LineEdit.clear()
+
+        elif self.keyLineEdit.text() != self.confirmLineEdit.text():
+            QMessageBox.warning(self, 'Warning', '两次密码不一致')
+            self.keyLineEdit.clear()
+            self.confirmLineEdit.clear()
--- E-commerceMIS/signInFunction.py
+++ E-commerceMIS/signInFunction.py
@@ -1,18 +1,21 @@
 import sys
 from PyQt5 import QtWidgets
 from signIn import Ui_Form
+from forgotDialogFunction import forgotDialog
 
 
 class SignIn(QtWidgets.QWidget, Ui_Form):
 
-    def __init__(self):
+    def __init__(self, db, cursor):
         super(SignIn, self).__init__()
         self.setupUi(self)
 
-        __sql = ''
+        self.sql = ''
+        self.db = db
+        self.dbcursor = cursor
 
         self.chooseComboBox.currentIndexChanged.connect(self.comboBoxChanged)
-        self.forgotPushButton.clicked.connect()
+        self.forgotPushButton.clicked.connect(self.forgotKey)
 
     def comboBoxChanged(self):
         if self.chooseComboBox.currentIndex() == 2:
@@ -23,12 +26,33 @@ class SignIn(QtWidgets.QWidget, Ui_Form):
             self.signUpPushButton.setEnabled(True)
 
     def forgotKey(self):
-        
+        if self.nameLineEdit.text() == '':
+            QtWidgets.QMessageBox.warning(self, 'Warning', '昵称不能为空')
+        else:
+            if self.chooseComboBox.currentIndex() == 0:
+                self.sql = 'select BkeyProtect1, BkeyAns1, BkeyProtect2, BkeyAns2 from buyer where Bname = \"' \
+                           + self.nameLineEdit.text() + '\"'
+            elif self.chooseComboBox.currentIndex() == 1:
+                self.sql = 'select BkeyProtect1, BkeyAns1, BkeyProtect2, BkeyAns2 from store where Sname = \"' \
+                           + self.nameLineEdit.text() + '\"'
+            self.dbcursor.execute(self.sql)
+            self.data = self.dbcursor.fetchone()
+            if self.data == None:
+                QtWidgets.QMessageBox.warning(self, 'Warning', '账户不存在')
+                self.nameLineEdit.clear()
+            else:
+                self.forgotDialog = forgotDialog(self.db, self.dbcursor, self.chooseComboBox.currentIndex(),
+                                                 self.nameLineEdit.text(), self.data)
+                self.forgotDialog.show()
+
+    def buyersignUp(self):
+        pass
 
 
 
-def showSignInUI():
+
+def showSignInUI(db, cursor):
         app = QtWidgets.QApplication(sys.argv)
-        signInUI = SignIn()
+        signInUI = SignIn(db, cursor)
         signInUI.show()
         sys.exit(app.exec_())
