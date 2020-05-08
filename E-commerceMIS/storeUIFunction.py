a16d7caceb44dff6740320744421f571febc8b5f
Ziye <504250110@qq.com>
2020-05-09 03:04:59 +0800

    Update

--- E-commerceMIS/storeUIFunction.py
+++ E-commerceMIS/storeUIFunction.py
@@ -16,68 +16,247 @@ class StoreUI(QtWidgets.QWidget, Ui_Form):
 
         self.commodityTableWidget.setColumnWidth(0, 40)
         self.commodityTableWidget.setColumnWidth(1, 80)
+        self.commodityTableWidget.setColumnWidth(2, 150)
+        self.commodityTableWidget.setColumnWidth(3, 60)
+        self.commodityTableWidget.setColumnWidth(4, 60)
+        self.commodityTableWidget.setColumnWidth(5, 60)
+        self.commodityTableWidget.setColumnWidth(6, 60)
+        self.commodityTableWidget.setColumnWidth(7, 200)
 
-        self.widget1 = QtWidgets.QWidget()
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget1)
-        spacerItem1 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem1)
-        self.checkBox1 = QtWidgets.QCheckBox(self.widget1)
-        self.horizontalLayout.addWidget(self.checkBox1)
-        spacerItem2 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem2)
-        self.commodityTableWidget.setCellWidget(0, 0, self.widget1)
-        self.textBrowser1 = QtWidgets.QTextBrowser()
-        self.commodityTableWidget.setCellWidget(0, 2, self.textBrowser1)
-        self.textBrowser4 = QtWidgets.QTextBrowser()
-        self.commodityTableWidget.setCellWidget(0, 7, self.textBrowser4)
+        self.getAllData()
 
-        self.widget2 = QtWidgets.QWidget()
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget2)
-        spacerItem3 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem3)
-        self.checkBox2 = QtWidgets.QCheckBox(self.widget2)
-        self.horizontalLayout.addWidget(self.checkBox2)
-        spacerItem4 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem4)
-        self.commodityTableWidget.setCellWidget(1, 0, self.widget2)
-        self.textBrowser2 = QtWidgets.QTextBrowser()
-        self.commodityTableWidget.setCellWidget(1, 2, self.textBrowser2)
-        self.textBrowser5 = QtWidgets.QTextBrowser()
-        self.commodityTableWidget.setCellWidget(1, 7, self.textBrowser5)
-
-        self.widget3 = QtWidgets.QWidget()
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget3)
-        spacerItem5 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem5)
-        self.checkBox3 = QtWidgets.QCheckBox(self.widget3)
-        self.horizontalLayout.addWidget(self.checkBox3)
-        spacerItem6 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem6)
-        self.commodityTableWidget.setCellWidget(2, 0, self.widget3)
-        self.textBrowser3 = QtWidgets.QTextBrowser()
-        self.commodityTableWidget.setCellWidget(2, 2, self.textBrowser3)
-        self.textBrowser6 = QtWidgets.QTextBrowser()
-        self.commodityTableWidget.setCellWidget(2, 7, self.textBrowser6)
-
-        self.sql = 'select Cname, Cdescribe, price, discount, commodity.stockSize, stock, address ' \
-                   'from commodity, warehouse where commodity.Sid = \"' + self.Sid \
-                   + '\" and commodity.Cid = warehouse.stockCid and commodity.stockSize = warehouse.stockSize;'
-        self.dbcursor.execute(self.sql)
-        self.data = self.dbcursor.fetchall()
+        self.initTable()
 
         self.commodityPageLabel.setText(str(self.page))
         self.enablePagePushButton()
-        self.enableFuncPushButton()
-        self.showData()
+        self.reShow()
+
+        self.sql = 'select Sname, Scredit, principal, Tel, ID_card ' \
+                   'from store where Sid = \"' + self.Sid + '\";'
+        self.dbcursor.execute(self.sql)
+        self.info = self.dbcursor.fetchone()
+
+        self.SnameLineEdit.setText(self.info[0])
+        self.creditLabel.setText(str(self.info[1]))
+        self.principalLineEdit.setText(self.info[2])
+        self.TelLineEdit.setText(self.info[3])
+        self.IDLineEdit.setText(self.info[4])
 
         self.commodityAddPushButton.clicked.connect(self.addCommodity)
-        self.checkBox1.stateChanged.connect(self.enableFuncPushButton)
-        self.checkBox2.stateChanged.connect(self.enableFuncPushButton)
-        self.checkBox3.stateChanged.connect(self.enableFuncPushButton)
+        self.commodityUpdatePushButton.clicked.connect(self.updataCommodity)
+        self.commodityDeletePushButton.clicked.connect(self.deleteCommodity)
+        self.commoditySearchPushButton.clicked.connect(self.searchCommodity)
+
+        self.commodityLastPushButton.clicked.connect(self.lastPage)
+        self.commodityNextPushButton.clicked.connect(self.nextPage)
+
+        self.infoUpdatePushButton.clicked.connect(self.updateInfo)
+
+    def searchCommodity(self):
+        self.sql = 'select Cname, Cdescribe, price, discount, commodity.stockSize, stock, address, Cid, warehouse.Wid ' \
+                   'from commodity, warehouse where commodity.Sid = \"' + self.Sid \
+                   + '\" and commodity.Cid = warehouse.stockCid and commodity.stockSize = warehouse.stockSize ' \
+                     'and Cname like \"%' + self.commoditySearchLineEdit.text() + '%\";'
+        self.dbcursor.execute(self.sql)
+        self.data = self.dbcursor.fetchall()
+        self.page = 1
+        self.reShow()
 
     def addCommodity(self):
         self.addCommodityDialog = AddCommodityDialog(self.db, self.dbcursor, self.Sid)
         self.addCommodityDialog.show()
+        self.getAllData()
+        self.page = 1
+        self.reShow()
+
+    def updataCommodity(self):
+        if self.page * 3 - 3 + 0 < len(self.data) and self.checkBox1.isChecked():
+            self.sql = 'update warehouse set stockSize = \"' + self.commodityTableWidget.item(0, 5).text() \
+                       + '\", stock = \"' + self.commodityTableWidget.item(0, 6).text() \
+                       + '\" where stockCid = \"' + self.data[self.page * 3 - 3 + 0][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 0][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+            self.sql = 'update commodity set stockSize = \"' + self.commodityTableWidget.item(0, 5).text() \
+                       + '\", Cname = \"' + self.commodityTableWidget.item(0, 1).text() \
+                       + '\", price = \"' + self.commodityTableWidget.item(0, 3).text() \
+                       + '\", discount = \"' + self.commodityTableWidget.item(0, 4).text() \
+                       + '\", Cdescribe = \"' + self.textBrowser1.toPlainText() \
+                       + '\" where Cid = \"' + self.data[self.page * 3 - 3 + 0][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 0][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+        if self.page * 3 - 3 + 1 < len(self.data) and self.checkBox2.isChecked():
+            self.sql = 'update warehouse set stockSize = \"' + self.commodityTableWidget.item(1, 5).text() \
+                       + '\", stock = \"' + self.commodityTableWidget.item(1, 6).text() \
+                       + '\" where stockCid = \"' + self.data[self.page * 3 - 3 + 1][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 1][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+            self.sql = 'update commodity set stockSize = \"' + self.commodityTableWidget.item(1, 5).text() \
+                       + '\", Cname = \"' + self.commodityTableWidget.item(1, 1).text() \
+                       + '\", price = \"' + self.commodityTableWidget.item(1, 3).text() \
+                       + '\", discount = \"' + self.commodityTableWidget.item(1, 4).text() \
+                       + '\", Cdescribe = \"' + self.textBrowser2.toPlainText() \
+                       + '\" where Cid = \"' + self.data[self.page * 3 - 3 + 1][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 1][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+        if self.page * 3 - 3 + 2 < len(self.data) and self.checkBox3.isChecked():
+            self.sql = 'update warehouse set stockSize = \"' + self.commodityTableWidget.item(2, 5).text() \
+                       + '\", stock = \"' + self.commodityTableWidget.item(2, 6).text() \
+                       + '\" where stockCid = \"' + self.data[self.page * 3 - 3 + 2][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 2][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+            self.sql = 'update commodity set stockSize = \"' + self.commodityTableWidget.item(2, 5).text() \
+                       + '\", Cname = \"' + self.commodityTableWidget.item(2, 1).text() \
+                       + '\", price = \"' + self.commodityTableWidget.item(2, 3).text() \
+                       + '\", discount = \"' + self.commodityTableWidget.item(2, 4).text() \
+                       + '\", Cdescribe = \"' + self.textBrowser3.toPlainText() \
+                       + '\" where Cid = \"' + self.data[self.page * 3 - 3 + 2][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 2][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+        QtWidgets.QMessageBox.information(self, 'Information', '修改成功')
+
+    def deleteCommodity(self):
+        if self.page * 3 - 3 + 0 < len(self.data) and self.checkBox1.isChecked():
+            self.sql = 'delete from commodity where Cid = \"' + self.data[self.page * 3 - 3 + 0][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 0][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+            self.sql = 'delete from warehouse where Wid = \"' + self.data[self.page * 3 - 3 + 0][8] \
+                       + '\" and stockCid = \"' + self.data[self.page * 3 - 3 + 0][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 0][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+        if self.page * 3 - 3 + 1 < len(self.data) and self.checkBox2.isChecked():
+            self.sql = 'delete from commodity where Cid = \"' + self.data[self.page * 3 - 3 + 1][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 1][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+            self.sql = 'delete from warehouse where Wid = \"' + self.data[self.page * 3 - 3 + 1][8] \
+                       + '\" and stockCid = \"' + self.data[self.page * 3 - 3 + 1][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 1][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+        if self.page * 3 - 3 + 2 < len(self.data) and self.checkBox3.isChecked():
+            self.sql = 'delete from commodity where Cid = \"' + self.data[self.page * 3 - 3 + 2][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 2][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+            self.sql = 'delete from warehouse where Wid = \"' + self.data[self.page * 3 - 3 + 2][8] \
+                       + '\" and stockCid = \"' + self.data[self.page * 3 - 3 + 2][7] \
+                       + '\" and stockSize = \"' + self.data[self.page * 3 - 3 + 2][4] + '\";'
+
+            self.dbcursor.execute(self.sql)
+            self.db.commit()
+
+        QtWidgets.QMessageBox.information(self, 'Information', '删除成功')
+        self.getAllData()
+        self.page = 1
+        self.reShow()
+
+    def lastPage(self):
+        self.page = self.page - 1
+        self.showData()
+        self.showPage()
+        self.clearCheckBox()
+        self.enablePagePushButton()
+        self.enableFuncPushButton()
+
+    def nextPage(self):
+        self.page = self.page + 1
+        self.showData()
+        self.showPage()
+        self.clearCheckBox()
+        self.enablePagePushButton()
+        self.enableFuncPushButton()
+
+    def updateInfo(self):
+        self.sql = 'update store set Sname = \"' + self.SnameLineEdit.text() + '\", principal = \"' \
+                   + self.principalLineEdit.text() + '\", Tel = \"' + self.TelLineEdit.text() \
+                   + '\", ID_card = \"' + self.IDLineEdit.text() + '\" where Sid = \"' + self.Sid + '\";'
+        self.dbcursor.execute(self.sql)
+        self.db.commit()
+
+        QtWidgets.QMessageBox.information(self, 'Information', '修改成功')
+
+    def initTable(self):
+        if self.page * 3 - 3 + 0 < len(self.data):
+            self.widget1 = QtWidgets.QWidget()
+            self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget1)
+            spacerItem1 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+            self.horizontalLayout.addItem(spacerItem1)
+            self.checkBox1 = QtWidgets.QCheckBox(self.widget1)
+            self.horizontalLayout.addWidget(self.checkBox1)
+            spacerItem2 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+            self.horizontalLayout.addItem(spacerItem2)
+            self.commodityTableWidget.setCellWidget(0, 0, self.widget1)
+            self.textBrowser1 = QtWidgets.QTextBrowser()
+            self.commodityTableWidget.setCellWidget(0, 2, self.textBrowser1)
+            self.textBrowser4 = QtWidgets.QTextBrowser()
+            self.commodityTableWidget.setCellWidget(0, 7, self.textBrowser4)
+            self.textBrowser1.setReadOnly(False)
+
+        if self.page * 3 - 3 + 1 < len(self.data):
+            self.widget2 = QtWidgets.QWidget()
+            self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget2)
+            spacerItem3 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+            self.horizontalLayout.addItem(spacerItem3)
+            self.checkBox2 = QtWidgets.QCheckBox(self.widget2)
+            self.horizontalLayout.addWidget(self.checkBox2)
+            spacerItem4 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+            self.horizontalLayout.addItem(spacerItem4)
+            self.commodityTableWidget.setCellWidget(1, 0, self.widget2)
+            self.textBrowser2 = QtWidgets.QTextBrowser()
+            self.commodityTableWidget.setCellWidget(1, 2, self.textBrowser2)
+            self.textBrowser5 = QtWidgets.QTextBrowser()
+            self.commodityTableWidget.setCellWidget(1, 7, self.textBrowser5)
+            self.textBrowser2.setReadOnly(False)
+
+        if self.page * 3 - 3 + 2 < len(self.data):
+            self.widget3 = QtWidgets.QWidget()
+            self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget3)
+            spacerItem5 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+            self.horizontalLayout.addItem(spacerItem5)
+            self.checkBox3 = QtWidgets.QCheckBox(self.widget3)
+            self.horizontalLayout.addWidget(self.checkBox3)
+            spacerItem6 = QtWidgets.QSpacerItem(30, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+            self.horizontalLayout.addItem(spacerItem6)
+            self.commodityTableWidget.setCellWidget(2, 0, self.widget3)
+            self.textBrowser3 = QtWidgets.QTextBrowser()
+            self.commodityTableWidget.setCellWidget(2, 2, self.textBrowser3)
+            self.textBrowser6 = QtWidgets.QTextBrowser()
+            self.commodityTableWidget.setCellWidget(2, 7, self.textBrowser6)
+            self.textBrowser3.setReadOnly(False)
+
+        self.checkBox1.stateChanged.connect(self.enableFuncPushButton)
+        self.checkBox2.stateChanged.connect(self.enableFuncPushButton)
+        self.checkBox3.stateChanged.connect(self.enableFuncPushButton)
 
     def enablePagePushButton(self):
         if self.page == 1:
@@ -91,30 +270,55 @@ class StoreUI(QtWidgets.QWidget, Ui_Form):
             self.commodityNextPushButton.setEnabled(True)
 
     def enableFuncPushButton(self):
+        self.commodityAddPushButton.setEnabled(True)
         if self.checkBox1.isChecked() or self.checkBox2.isChecked() or self.checkBox3.isChecked():
-            self.commodityAddPushButton.setEnabled(True)
             self.commodityUpdatePushButton.setEnabled(True)
             self.commodityDeletePushButton.setEnabled(True)
         else:
-            self.commodityAddPushButton.setEnabled(False)
             self.commodityUpdatePushButton.setEnabled(False)
             self.commodityDeletePushButton.setEnabled(False)
 
-    def showData(self):
-        print(self.data)
-        for i in range(0, 3):
-            for j in [0, 2, 3, 4, 5, 6]:
-                self.commodityTableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(self.data[self.page * 3 - 3 + i][j])))
-            if i == 0:
-                self.textBrowser1.setText(self.data[self.page * 3 - 3 + i][1])
-                self.textBrowser4.setText(self.data[self.page * 3 - 3 + i][7])
-            elif i == 1:
-                self.textBrowser2.setText(self.data[self.page * 3 - 3 + i][1])
-                self.textBrowser5.setText(self.data[self.page * 3 - 3 + i][7])
-            elif i == 2:
-                self.textBrowser3.setText(self.data[self.page * 3 - 3 + i][2])
-                self.textBrowser6.setText(self.data[self.page * 3 - 3 + i][7])
+    def getAllData(self):
+        self.sql = 'select Cname, Cdescribe, price, discount, commodity.stockSize, stock, address, Cid, warehouse.Wid ' \
+                   'from commodity, warehouse where commodity.Sid = \"' + self.Sid \
+                   + '\" and commodity.Cid = warehouse.stockCid and commodity.stockSize = warehouse.stockSize;'
+        self.dbcursor.execute(self.sql)
+        self.data = self.dbcursor.fetchall()
 
+    def showData(self):
+        self.clearTable()
+        for i in range(0, 3):
+            if self.page * 3 - 3 + i < len(self.data):
+                for j in [1, 3, 4, 5, 6]:
+                    self.commodityTableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(self.data[self.page * 3 - 3 + i][j - 1])))
+                if i == 0:
+                    self.textBrowser1.setText(self.data[self.page * 3 - 3 + i][1])
+                    self.textBrowser4.setText(self.data[self.page * 3 - 3 + i][6])
+                elif i == 1:
+                    self.textBrowser2.setText(self.data[self.page * 3 - 3 + i][1])
+                    self.textBrowser5.setText(self.data[self.page * 3 - 3 + i][6])
+                elif i == 2:
+                    self.textBrowser3.setText(self.data[self.page * 3 - 3 + i][1])
+                    self.textBrowser6.setText(self.data[self.page * 3 - 3 + i][6])
+            else:
+                break
+
+    def showPage(self):
+        self.commodityPageLabel.setText('第' + str(self.page) + '页')
+
+    def reShow(self):
+        self.showData()
+        self.enableFuncPushButton()
+        self.showPage()
+
+    def clearCheckBox(self):
+        self.checkBox1.setCheckState(False)
+        self.checkBox2.setCheckState(False)
+        self.checkBox3.setCheckState(False)
+
+    def clearTable(self):
+        self.commodityTableWidget.clearContents()
+        self.initTable()
 
 
 if __name__ == "__main__":
