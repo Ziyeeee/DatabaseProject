c88fcb9522fa469215829ad2a004d7b2d6d7306f
Ziye <504250110@qq.com>
2020-05-23 23:51:26 +0800

    Update experiment3

--- extmem/experiment3/test.c
+++ extmem/experiment3/test.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include "extmem.h"
 
+void createIndex(int startAddr, int numOfDataBlk, int selectNum);
+void select(int indexStartAddr, int writeAddr, int numOfDataBlk, int selectNum);
+
 int main(int argc, char **argv)
 {
     Buffer buf; /* A buffer */
@@ -14,61 +17,58 @@ int main(int argc, char **argv)
         perror("Buffer Initialization Failed!\n");
         return -1;
     }
-
-    /* Get a new block in the buffer */
-    blk = getNewBlockInBuffer(&buf);
-
-    /* Fill data into the block */
-    for (i = 0; i < 8; i++)
-        *(blk + i) = 'a' + i;
-
-    /* Write the block to the hard disk */
-    if (writeBlockToDisk(blk, 8888, &buf) != 0)
-    {
-        perror("Writing Block Failed!\n");
-        return -1;
-    }
-
-    /* Read the block from the hard disk */
-    if ((blk = readBlockFromDisk(1, &buf)) == NULL)
-    {
-        perror("Reading Block Failed!\n");
-        return -1;
-    }
-
-    /* Process the data in the block */
-    int X = -1;
-    int Y = -1;
-    int addr = -1;
-
-    char str[5];
-    printf("block 1:\n");
-    for (i = 0; i < 7; i++) //一个blk存7个元组加一个地址
-    {
-
-        for (int k = 0; k < 4; k++)
-        {
-            str[k] = *(blk + i*8 + k);
-        }
-        X = atoi(str);
-        for (int k = 0; k < 4; k++)
-        {
-            str[k] = *(blk + i*8 + 4 + k);
-        }
-        Y = atoi(str);
-        printf("(%d, %d) ", X, Y);
-    }
-    for (int k = 0; k < 4; k++)
-    {
-        str[k] = *(blk + i*8 + k);
-    }
-    addr = atoi(str);
-    printf("\nnext address = %d \n", addr);
-
-
-    printf("\n");
-    printf("IO's is %d\n", buf.numIO); /* Check the number of IO's */
-
     return 0;
 }
 
+
+void createIndex(int startReadAddr, int numOfDataBlk, int startWriteAddr, int selectNum)
+{
+    Buffer buf; /* A buffer */
+    unsigned char *dataBlk, *indexBlk; /* A pointer to a block */
+    int i, j, k;
+    int readAddr = startReadAddr;
+    int writeAddr = startWriteAddr;
+    char strAddr[5];
+
+    /* Initialize the buffer */
+    if (!initBuffer(520, 64, &buf))
+    {
+        perror("Buffer Initialization Failed!\n");
+        return -1;
+    }
+
+    printf("将索引块写入到");
+    for(i = 0; i < numOfDataBlk / 8 < i++)
+    {
+        indexBlk = getNewBlockInBuffer(&buf);
+        for(j = 0; j < 8; i++)
+        {
+            dataBlk = readBlockFromDisk(readAddr, &buf);
+
+            *(indexBlk + i * 8 + 0) = *(dataBlk + 0);
+            *(indexBlk + i * 8 + 1) = *(dataBlk + 1);
+            *(indexBlk + i * 8 + 2) = *(dataBlk + 6 * 8 + 0);
+            *(indexBlk + i * 8 + 3) = *(dataBlk + 6 * 8 + 1);
+            itoa(readAddr, strAddr, 10);
+            for (k = 0; k < 4; k++)
+            {
+                *(indexBlk + i * 8 + k + 4) = strAddr[k];
+            }
+
+            readAddr++;
+            freeBlockInBuffer(dataBlk, &buf);
+        }
+        writeBlockToDisk(indexBlk, writeAddr, &buf);
+        printf("%d ");
+        writeAddr++;
+    }
+    printf("磁盘块中\n");
+
+    select(startWriteAddr, 100 + startWriteAddr, numOfDataBlk, selectNum);
+    return;
+}
+
+void select(int indexStartAddr, int writeAddr, int numOfDataBlk, int selectNum)
+{
+
+}
