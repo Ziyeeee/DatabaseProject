77c90734c5587bd11317bf83bc4778a1d8623c57
Ziye <504250110@qq.com>
2020-05-24 17:12:16 +0800

    Update experiment5

--- extmem/experiment5/test.c
+++ extmem/experiment5/test.c
@@ -2,73 +2,168 @@
 #include <stdio.h>
 #include "extmem.h"
 
+void sort(unsigned char *blk[8], int numOfMember);
+
 int main(int argc, char **argv)
 {
     Buffer buf; /* A buffer */
-    unsigned char *blk; /* A pointer to a block */
-    int i = 0;
+    unsigned char *blk[8] = {NULL}; /*pointers to a block */
+    int numOfMember = 4;
+    int addr = 1;
+    int writeAddr = 101;
+    int i, j, k;
+    char str[5];
+    int blkCount[5] = {0};
 
     /* Initialize the buffer */
-    if (!initBuffer(520, 64, &buf))
+    if(!initBuffer(520, 64, &buf))
     {
         perror("Buffer Initialization Failed!\n");
         return -1;
     }
 
-    /* Get a new block in the buffer */
-    blk = getNewBlockInBuffer(&buf);
-
-    /* Fill data into the block */
-    for (i = 0; i < 8; i++)
-        *(blk + i) = 'a' + i;
-
-    /* Write the block to the hard disk */
-    if (writeBlockToDisk(blk, 8888, &buf) != 0)
+    printf("对关系R的子表按R.A从大到小顺序进行排序：\n");
+    printf("将进行组内排序后的数据写入到");
+    for(i = 0; i < 4; i++)
     {
-        perror("Writing Block Failed!\n");
+        for(j = 0; j < numOfMember; j++)
+        {
+            blk[j] = readBlockFromDisk(addr, &buf);
+            addr++;
+        }
+        sort(blk, numOfMember);
+        for(j = 0; j < numOfMember; j++)
+        {
+            itoa(writeAddr + 1, str, 10);
+            for(k = 0; k < 4; k++)
+            {
+                *(blk[j] + 8 * 7 + k) = str[k];
+            }
+            if(writeBlockToDisk(blk[j], writeAddr, &buf) == 0)
+            {
+                printf("%d ", writeAddr);
+                writeAddr++;
+            }
+            freeBlockInBuffer(blk[j], &buf);
+        }
+    }
+    printf("磁盘块中\n");
+    freeBuffer(&buf);
+
+    numOfMember = 8;
+    addr = 17;
+    writeAddr = 117;
+    for(i = 0; i < 5; i++)
+    {
+        blkCount[i] = 0;
+    }
+
+    /* Initialize the buffer */
+    if(!initBuffer(520, 64, &buf))
+    {
+        perror("Buffer Initialization Failed!\n");
         return -1;
     }
 
-    /* Read the block from the hard disk */
-    if ((blk = readBlockFromDisk(1, &buf)) == NULL)
+    printf("\n对关系S的子表按S.C从大到小顺序进行排序：\n");
+    printf("将进行组内排序后的数据写入到");
+    for(i = 0; i < 4; i++)
     {
-        perror("Reading Block Failed!\n");
-        return -1;
-    }
-
-    /* Process the data in the block */
-    int X = -1;
-    int Y = -1;
-    int addr = -1;
-
-    char str[5];
-    printf("block 1:\n");
-    for (i = 0; i < 7; i++) //一个blk存7个元组加一个地址
-    {
-
-        for (int k = 0; k < 4; k++)
+        for(j = 0; j < numOfMember; j++)
         {
-            str[k] = *(blk + i*8 + k);
+            blk[j] = readBlockFromDisk(addr, &buf);
+            addr++;
         }
-        X = atoi(str);
-        for (int k = 0; k < 4; k++)
+        sort(blk, numOfMember);
+        for(j = 0; j < numOfMember; j++)
         {
-            str[k] = *(blk + i*8 + 4 + k);
+            itoa(writeAddr + 1, str, 10);
+            for(k = 0; k < 4; k++)
+            {
+                *(blk[j] + 8 * 7 + k) = str[k];
+            }
+            if(writeBlockToDisk(blk[j], writeAddr, &buf) == 0)
+            {
+                printf("%d ", writeAddr);
+                writeAddr++;
+            }
+            freeBlockInBuffer(blk[j], &buf);
         }
-        Y = atoi(str);
-        printf("(%d, %d) ", X, Y);
     }
-    for (int k = 0; k < 4; k++)
-    {
-        str[k] = *(blk + i*8 + k);
-    }
-    addr = atoi(str);
-    printf("\nnext address = %d \n", addr);
-
-
-    printf("\n");
-    printf("IO's is %d\n", buf.numIO); /* Check the number of IO's */
-
+    printf("磁盘块中\n");
+    freeBuffer(&buf);
     return 0;
 }
 
+void sort(unsigned char *blk[8], int numOfMember)
+{
+    int i, j, k, m, n;
+    int X, Y;
+    int maxPosi, maxPosj, unchanged;
+    char str[5];
+    char temp;
+
+    for(i = 0; i < numOfMember; i++)
+    {
+        for(j = 0; j < 7; j++)
+        {
+            for(k = 0; k < 4; k++)
+            {
+                str[k] = *(blk[i] + j*8 + k);
+            }
+            X = atoi(str);
+            unchanged = 1;
+
+            n = j + 1;
+            for(m = i; m < numOfMember; m++)
+            {
+                while(n < 7)
+                {
+                    for(k = 0; k < 4; k++)
+                    {
+                        str[k] = *(blk[m] + n*8 + k);
+                    }
+                    Y = atoi(str);
+                    if(Y > X)
+                    {
+                        maxPosi = m;
+                        maxPosj = n;
+                        X = Y;
+                        unchanged = 0;
+                        // printf("%d, %d ", m, n);
+                    }
+                    n++;
+                }
+                n = 0;
+            }
+            // printf("\n%d, %d\n", maxPosi, maxPosj);
+            if(unchanged == 0)
+            {
+                for(k = 0; k < 8; k++)
+                {
+                    temp = *(blk[i] + j*8 + k);
+                    *(blk[i] + j*8 + k) = *(blk[maxPosi] + maxPosj*8 + k);
+                    *(blk[maxPosi] + maxPosj*8 + k) = temp;
+                }
+            }
+            // printf("%d ", X);
+        }
+        // printf("\n");
+    }
+/*  查看排序结果
+    for(i = 0; i < numOfMember; i++)
+    {
+        for(j = 0; j < 7; j++)
+        {
+            for(k = 0; k < 4; k++)
+            {
+                str[k] = *(blk[i] + j*8 + k);
+            }
+            X = atoi(str);
+            printf("%d ", X);
+        }
+    }
+    printf("\n");
+*/
+    return;
+}
